9752a266865431d9f084ce72ed3da9cd061929be
Ria <riaratnasari@outlook.com>
2019-04-28 22:06:41 +0200

    add venue info

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './resources/styles.css';
 import Header from './component/header_footer/Header';
 import Featured from './component/featured';
+import VenueInfo from './component/venueInfo';
 function App() {
   return (
     <div className="App" style={{height: '3000px', backgroundColor: '#efefef'}}>
       <Header />
       <Featured />
+      <VenueInfo />
     </div>
   );
 }
